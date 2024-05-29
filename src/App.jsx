8d22befe2ebb46cd751840adeec1b842cb69be34
Azamat Ochilov <azikcode.blog@gmail.com>
2024-05-29 17:31:47 +0500

    change bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ export default function App() {
   const [inp8, set8] = useState("");
   const [inp9, set9] = useState("");
   const [inp10, set10] = useState("");
-
+  console.log(import.meta.env.VITE_BOT_TOKEN);
+  console.log(import.meta.env.VITE_CHAT_ID);
   const HandleSubmitForm = async (e) => {
     e.preventDefault();
     function formatUserName (username){
@@ -27,35 +28,36 @@ export default function App() {
   }
     const message =
       `Новая идея проекта:\n\n` +
-      `*Никнейм*: ${formatUserName(inp1)}\n\n` +
-      `*Идея*: ${inp2}\n\n` +
-      `*Проблемы*: ${inp3}\n\n` +
-      `*Целевая аудитория*: ${inp4}\n\n` +
-      `*Инновации*: ${inp5}\n\n` +
-      `*Масштабирование*: ${inp6}\n\n` +
-      `*Этапы реализации*: ${inp7}\n\n` +
-      `*Коммерческий потенциал*: ${inp8}\n\n` +
-      `*Аналогичные проекты*: ${inp9}\n\n` +
-      `*Участие сообщества*: ${inp10}`;
+      `Никнейм: ${formatUserName(inp1)}\n\n` +
+      `Идея: ${inp2}\n\n` +
+      `Проблемы: ${inp3}\n\n` +
+      `Целевая аудитория: ${inp4}\n\n` +
+      `Инновации: ${inp5}\n\n` +
+      `Масштабирование: ${inp6}\n\n` +
+      `Этапы реализации: ${inp7}\n\n` +
+      `Коммерческий потенциал: ${inp8}\n\n` +
+      `Аналогичные проекты: ${inp9}\n\n` +
+      `Участие сообщества: ${inp10}`;
 
     if (inp1 && inp2 && inp3 && inp4 && inp5 && inp6 && inp7 && inp8 && inp10) {
-      fetch(
-        `https://api.telegram.org/bot${
-          import.meta.env.VITE_BOT_TOKEN
-        }/sendMessage`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            chat_id: import.meta.env.VITE_CHAT_ID,
-            text: message,
-            parse_mode: "Markdown",
-          }),
-        }
-      )
-        .then((response) => response.json())
+      fetch(`https://api.telegram.org/bot${import.meta.env.VITE_BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: import.meta.env.VITE_CHAT_ID,
+          text: message,
+        }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.ok) {
+            console.log("Message sent successfully:", data);
+          } else {
+            console.error("Error sending message:", data);
+          }
+        })
         .then(() => {
           setModalMsg('Форма успешно отправлена!')
           setIsModal(true);
@@ -71,7 +73,7 @@ export default function App() {
           set10("");
           setTimeout(() => setIsModal(false), 3000)
         })
-        .catch((error) => {
+        .catch((error , data) => {
           console.log(error.message);
         });
     } else {
